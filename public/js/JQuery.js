f32029f1d505d1784e5095b8fdf129213a483ef9
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-08-22 19:49:42 -0700

    got the multiple images working with the page

---
+++ public/js/JQuery.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    // all custom jQuery will go here
+    const images = [
+
+  'http://cd.visitmelbourne.com/-/media/images/great-ocean-road/things-to-do/outdoor-activities/walking-and-hiking/hiking-great-ocean-walk_gor_r_1374435_1150x863.jpg?ts=20150625031118&w=480&h=360&crop=1|http://cd.visitmelbourne.com/-/media/images/great-ocean-road/things-to-do/outdoor-activities/walking-and-hiking/hiking-great-ocean-walk_gor_r_1374435_1150x863.jpg?ts=20150625031118&w=720&h=540&crop=1',
+  'https://static.pexels.com/photos/33109/fall-autumn-red-season.jpg',
+  'http://az616578.vo.msecnd.net/files/2016/06/25/6360243057518096701592067658_191013.jpg',
+  'http://az616578.vo.msecnd.net/files/2016/06/30/636028626429115563-686613358_hikers3.jpg'
+
+];
+
+let index = 0;
+
+setInterval(change_up, 10000);
+
+function change_up(){
+
+  index = (index + 1 < images.length) ? index + 1 : 0;
+
+  $('.block').fadeOut(3000, function(){
+
+    $(this).css('background-image', 'url('+ images[index] + ')')
+
+    $(this).fadeIn(3000);
+
+  });
+}
+
+
+});
